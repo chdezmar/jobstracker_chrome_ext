e948b4ba47befceb9a636518c403317afc9deaaa
Carlos <chdezmar@gmail.com>
2016-09-16 16:46:07 +0100

    improvements

--- popup.js
+++ popup.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
 
   $('#add-job').click(function(event) {
       event.preventDefault();
-      data = {job: {url: $('#url').val(), company: $('#company').val(), position: $('#position').val()}};
+      data = {job: {url: $('#url').val(), company: $('#company').val(), position: $('#position').val(), position: $('#position').val(),location: $('#location').val(),status: $('#status').val(),rating: $('#rating').val(), info: $('#info').val()}};
 
       $.ajax({
         method: "POST",
@@ -17,9 +17,13 @@ $(document).ready(function () {
         data: data,
 
         success: function() {
-          $('#title').val("");
-          $('#company').val("");
           $('#url').val("");
+          $('#company').val("");
+          $('#position').val("");
+          $('#location').val("");
+          $('#status').val("");
+          $('#rating').val("");
+          $('#info').val("");
         },
         error: function(data) {
           return false;
